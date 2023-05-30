41b2586c26e482c8980ecc9d90118c205e748ad2
Utkarsh Choudhary <72992574+UDogg@users.noreply.github.com>
2023-05-30 16:05:06 +0530

    Can create data with forms

--- djcrm/leads/views.py
+++ djcrm/leads/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, Agent
 from .forms import LeadForm
 
 def lead_list(request):
@@ -21,8 +21,27 @@ def lead_detail(request, pk):
     return render(request, "leads/lead_detail.html", context)
 
 def lead_create(request):
+    form = LeadForm
+    if request.method == "POST":
+        print("Receiving a POST request")
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            print("The form is valid")
+            print(form.cleaned_data)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                age=age,
+                agent=agent
+            )
+            print("Lead has been created")
+            return redirect("/leads")
     context = {
-        "form": LeadForm()
+        "form": form
     }
     return render(request, "leads/lead_create.html", context)
 
